614893252852e8b92d20cbcaec181cce7e6181bd
dhavalpurohit <dhavalpurohit.work@gmail.com>
2024-04-21 21:08:55 +0530

    fix: mail id

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form field values
     $name = $_POST['name'];
@@ -16,24 +13,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Set headers
     $headers = "From: $name <$email>";
 
-    // Attempt to send email using SMTP
-   
-    $smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'info@elitebookdesign.com',
-        'password' => 'heyelite'
-    ));
-
-    $mail = $smtp->send($to, $headers, $body);
-
-    // Check if the email was sent successfully
-    if (PEAR::isError($mail)) {
-        echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
-        echo '<p>Error: ' . $mail->getMessage() . '</p>';
-    } else {
+    // Attempt to send email
+    if (mail($to, $subject, $body, $headers)) {
         echo '<p>Your message has been sent successfully!</p>';
+    } else {
+        echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
     }
 } else {
     // If the request method is not POST, redirect back to the contact page
