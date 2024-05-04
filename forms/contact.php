9555669265145e1211ef267660f856f0def68cab
dhavalpurohit <dhavalpurohit.work@gmail.com>
2024-05-04 20:46:24 +0530

    fix: contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,47 +1,20 @@
-<?php
-// Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+<!-- <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'info@elitebookdesign.com';
+    $subject = 'Contact Form Submission';
+    $body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $name <$email>";
 
-require_once 'vendor/autoload.php';
-
-$mail = new PHPMailer(true);
-
-//Configure SMTP
-$mail->isSMTP();
-$mail->Host = ‘smtp.gmail.com’;
-$mail->SMTPAuth = true;
-$mail->Username = ‘info@elitebookdesign.com’;
-$mail->Password = ‘App Password’;
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-// Sender information
-$mail->setFrom('elitebookdesign7@gmail.com', 'Dhaval');
-
-// Receiver email address and name
-$mail->addAddress('info@elitebookdesign.com', 'Elite Book Design'); 
-
-// Add cc or bcc   
-// $mail->addCC('email@mail.com');  
-// $mail->addBCC('user@mail.com');  
- 
- 
-$mail->isHTML(true); // Set email format to HTML
- 
-$mail->Subject = 'PHPMailer SMTP test';
-$mail->Body    = "<h1>PHPMailer the awesome Package</h1>
-                  <p>PHPMailer is working fine for sending mail.</p>
-                  <p>This is a tutorial to guide you on PHPMailer integration.</p>";
-
-// Attempt to send the email
-if (!$mail->send()) {
-    echo 'Email not sent. An error was encountered: ' . $mail->ErrorInfo;
+    if (mail($to, $subject, $body, $headers)) {
+        echo '<p>Your message has been sent successfully!</p>';
+    } else {
+        echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
+    }
 } else {
-    echo 'Message has been sent.';
+    header("Location: contact.php");
+    exit();
 }
-
-$mail->smtpClose();
-?>
-
+?> -->
