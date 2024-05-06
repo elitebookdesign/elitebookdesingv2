80ced6b7ad2a351fdd582c16b919c87271b3007c
dhavalpurohit <dhavalpurohit.work@gmail.com>
2024-05-06 23:48:53 +0530

    fix: contact form update

--- forms/contact.php
+++ forms/contact.php
@@ -1,20 +1,43 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $to = 'info@elitebookdesign.com';
-    $subject = 'Contact Form Submission';
-    $body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
-    $headers = "From: $name <$email>";
+use PHPMailer\PHPMailer;
+use PHPMailer\SMTP;
+use PHPMailer\Exception;
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo '<p>Main Mail successfully!</p>';
-    } else {
-        echo '<p>oh Sorry.</p>';
-    }
-} else {
-    header("Location: forms/contact.html");
-    exit();
+require './PHPMailer/PHPMailer.php';
+require './PHPMailer/SMTP.php';
+require './PHPMailer/Exception.php';
+if (isset($_POST['send'])) {
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@elitebookdesign.com';                     //SMTP username
+    $mail->Password   = 'rjltssglqydkekcd';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('info@elitebookdesign.com', 'Contact Form');
+    $mail->addAddress('elitebookdesign7@gmail.com', 'Elite Book');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = "Sender Name:- $name <br> Sender Mail:- $email <br> Sender Message: $message";
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error88: {$mail->ErrorInfo}";
 }
-?>
+}
